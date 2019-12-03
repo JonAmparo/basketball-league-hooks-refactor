2fac39cb6690ce283ad2231b4e0c07b6f7ad9d4a
JonAmparo <jonathanmamparo@gmail.com>
2019-12-02 19:41:04 -0500

    Updates

---
+++ precache-manifest.50641693acd2101ee85d5f4bd7fa9428.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4104e2a4351910688676baacc7f683",
+    "url": "/basketball-league-hooks-refactor/index.html"
+  },
+  {
+    "revision": "6644e3f7a4e9f60dfe3c",
+    "url": "/basketball-league-hooks-refactor/static/css/main.c84ec4ec.chunk.css"
+  },
+  {
+    "revision": "cba693e6fe04c5ced6f2",
+    "url": "/basketball-league-hooks-refactor/static/js/0.cf8cd960.chunk.js"
+  },
+  {
+    "revision": "3f956c6e5a44d530f593",
+    "url": "/basketball-league-hooks-refactor/static/js/1.e3ebc1e6.chunk.js"
+  },
+  {
+    "revision": "986f83c9d68b49728508",
+    "url": "/basketball-league-hooks-refactor/static/js/10.be89cac2.chunk.js"
+  },
+  {
+    "revision": "3d3bee2180c745ff0507",
+    "url": "/basketball-league-hooks-refactor/static/js/4.514cc34b.chunk.js"
+  },
+  {
+    "revision": "ac04009c142f2d9ec5a7",
+    "url": "/basketball-league-hooks-refactor/static/js/5.105a2337.chunk.js"
+  },
+  {
+    "revision": "3bc62c4729da9b9b9e3b",
+    "url": "/basketball-league-hooks-refactor/static/js/6.7b99e1c1.chunk.js"
+  },
+  {
+    "revision": "64bf93cafc97c5d95a9f",
+    "url": "/basketball-league-hooks-refactor/static/js/7.8bb693ae.chunk.js"
+  },
+  {
+    "revision": "ebe8a7047770f8885443",
+    "url": "/basketball-league-hooks-refactor/static/js/8.9e7a7712.chunk.js"
+  },
+  {
+    "revision": "20f2a70c442c563da52f",
+    "url": "/basketball-league-hooks-refactor/static/js/9.1f7d337a.chunk.js"
+  },
+  {
+    "revision": "6644e3f7a4e9f60dfe3c",
+    "url": "/basketball-league-hooks-refactor/static/js/main.8beb8ecb.chunk.js"
+  },
+  {
+    "revision": "3c854c78c7879ad997e4",
+    "url": "/basketball-league-hooks-refactor/static/js/runtime-main.f180a636.js"
+  }
+]);
